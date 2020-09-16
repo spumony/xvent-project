39fb7cf36a55ae39d24617788c82327213d3735b
Dino Spumony <aspumony@gmail.com>
2020-09-16 12:26:40 +0300

    Create a data connection to MongoDB with Mongoose

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mangoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mangoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
